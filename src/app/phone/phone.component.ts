3e7d9bcceca3da4fe2fec2aa74606c2e02494f7c
/ohnsonWTC <johnsondubula@gmail.com>
2022-11-12 14:39:01 +0200

    hide and show

--- src/app/phone/phone.component.ts
+++ src/app/phone/phone.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+
+export interface Phone{
+  name : string,
+  camera : string,
+  size : number
+
+}
+
 @Component({
   selector: 'app-phone',
   templateUrl: './phone.component.html',
@@ -7,9 +15,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhoneComponent implements OnInit {
 
+  showTableBtn = false;
+  showTableBtnText = "Hide table";
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  phones : Phone[] = [
+    {
+      name: "iPhone",
+      camera: "20 px",
+      size: 50
+    },
+    {
+      name: "Philips",
+      camera: "50 px",
+      size: 30
+    },
+    {
+      name: "Xiaomi",
+      camera: "120 px",
+      size: 150
+    }
+]
+
+  ShowTableAndHidetable(){
+    if(this.showTableBtn){
+      this.showTableBtn = false;
+      this.showTableBtnText = "Hide table";
+    }else{
+      this.showTableBtn = true;
+      this.showTableBtnText = "Show table";
+    }
   }
 
 }
